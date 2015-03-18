9a961afef466bcca95a3bc6fd3b7a7d253ad7bba
Diego Salazar <kirafreaky@gmail.com>
2015-03-18 00:02:42 -0500

    added stack of ideas
    Added localStorage support for ideas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,35 @@
 angular.module('mytaskrApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
 
-  	var tareasEnAlmacen = localStorageService.get('tareas'); 
-    $scope.tareas = tareasEnAlmacen && tareasEnAlmacen.split('\n') || [];
+  	var stackTareas = localStorageService.get('tareas'); 
+    var stackIdeas = localStorageService.get('ideas');
+
+    $scope.tareas = stackTareas && stackTareas.split('\n') || [];
+    $scope.ideas = stackIdeas && stackIdeas.split('\n') || [];
+
     $scope.$watch('tareas', function () {
        localStorageService.add('tareas', $scope.tareas.join('\n'));
        }, true);
 
+    $scope.$watch('ideas', function (){
+      localStorageService.add('ideas', $scope.ideas.join('\n'));
+    }, true);
+
+    $scope.addIdea = function () {
+      $scope.ideas.push($scope.idea);
+      $scope.idea = '';
+    };
+
     $scope.addTarea = function () {
     	$scope.tareas.push($scope.tarea);
-    	$scope.tarea = ' ';
+    	$scope.tarea = '';
     };
 
     $scope.eliminarTarea = function (index) {
     	$scope.tareas.splice(index, 1);
+    };
+
+    $scope.eliminarIdea = function (index) {
+      $scope.ideas.splice(index, 1);
     };
   });
