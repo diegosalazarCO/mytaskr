c951a79c7a1ea619155454c7a0220630d003d1e3
Diego Salazar <kirafreaky@gmail.com>
2015-03-18 18:06:17 -0500

    passing dictionary to LocalStorageService
    {titulo: “ ”, hecha: boolean}
    TODO: filter by active and done tasks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,23 +8,35 @@
  * Controller of the mytaskrApp
  */
 angular.module('mytaskrApp')
-  .controller('MainCtrl', function ($scope, localStorageService) {
+  .controller('MainCtrl', function ($scope, localStorageService, $filter) {
 
   	var stackTareas = localStorageService.get('tareas'); 
     var stackIdeas = localStorageService.get('ideas');
 
-    $scope.tareas = stackTareas && stackTareas.split('\n') || [];
+    //$scope.tareas = stackTareas || [];
     $scope.ideas = stackIdeas && stackIdeas.split('\n') || [];
-
+    $scope.tareas = (localStorage.getItem('tareas')!==null) ? JSON.parse($scope.stackTareas) : [ {titulo: 'Completar To dos de App', hecha: false}];
     $scope.estaOculta = true;
 
     $scope.$watch('tareas', function () {
-       localStorageService.add('tareas', $scope.tareas.join('\n'));
+       localStorageService.set('tareas', JSON.stringify($scope.tareas));
        }, true);
 
     $scope.$watch('ideas', function (){
       localStorageService.add('ideas', $scope.ideas.join('\n'));
     }, true);
+
+  //   $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Learn AngularJS', done: false}, {text: 'Build an Angular app', done: false} ];
+  // localStorage.setItem('todos', JSON.stringify($scope.todos));
+
+  // $scope.addTodo = function() {
+  //   $scope.todos.push({
+  //     text: $scope.todoText,
+  //     done: false
+  //   });
+  //   $scope.todoText = ''; //clear the input after adding
+  //   localStorage.setItem('todos', JSON.stringify($scope.todos));
+  // };
 
     $scope.addIdea = function () {
       $scope.ideas.push($scope.idea);
@@ -32,9 +44,17 @@ angular.module('mytaskrApp')
     };
 
     $scope.addTarea = function () {
-    	$scope.tareas.push($scope.tarea);
+
+      var nuevoParaHacer = {
+        titulo: $scope.tarea,
+        //haciendo: false,
+        hecha: false
+      };
+
+      $scope.tareas.push(nuevoParaHacer);
     	$scope.tarea = '';
       $scope.estaOculta = true;
+      localStorageService.add('tareas', JSON.stringify($scope.tareas));
     };
 
     $scope.eliminarTarea = function (index) {
@@ -47,5 +67,19 @@ angular.module('mytaskrApp')
 
     $scope.abrirTarea = function () {
       $scope.estaOculta = false;
+    };
+
+    $scope.tareaHecha = function (index) {
+      $scope.tareas[index].hecha = true;
+    };
+
+    $scope.filtroTareasHechas = function (elemento) {
+      console.log(elemento);
+      console.log(elemento.hecha);
+      return elemento.hecha;
+    };
+
+    $scope.eliminarTodasTareas = function () {
+      return localStorageService.remove('tareas');
     };
   });
